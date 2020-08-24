bf6c8542e86f49a0c59ac2118813d67cdb68ad22
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-24 13:05:39 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+// Write a JavaScript function that checks whether a passed string is palindrome or not? 
+
+function checkPalindrome(string){
+  // Check whether the string is empty or not
+    // if empty exit function and log your finding
+
+
+  // Change the string into lower case and remove all non-alphanumeric characters
+
+
+  // Check if the length of the string is even or odd 
+
+
+  // If the length of the string is 1 then it becomes a palindrome
+      // Exit function and log your finding
+
+
+  // If the length of the string is odd ignore middle character
+
+
+  // Loop through to check the first character to the last character and then move next
+
+
+  // Compare characters and drop them if they do not match 
+
+  // console log your findings
+}
